814a2691ce922aeb2812e3588675bc555c2d1937
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2021-01-12 00:48:58 +0530

    Diving Deeper Into the `request` object - Routing Requests using `request.url`

--- understanding-basics/src/index.ts
+++ understanding-basics/src/index.ts
@@ -5,23 +5,53 @@ import { IncomingMessage, ServerResponse } from 'http';
 import http from 'http';
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-  console.log(req.url, req.method, req.headers);
   /**
-   * We can log the response object (in this case, `res`) onto
-   * the console, but it actually doesn't contain any
-   * interesting data.
-   */
-  console.log(res); // ServerResponse object which contains many fields
-
-  /**
-   * Instead, we can use the response object to fill it with
-   * data we want to send back to the client.
-   *
-   * There are a couple of methods that are inbuilt in the
-   * response object which are response.setHeader(),
-   * response.write() & response.end(), etc.
+   * Instead of logging the `request` object, we can use it
+   * to create a webserver that does different things when 
+   * the user goes on different routes, i.e., /test, /message,
+   * /, /some-route, etc.
    */
 
+  /**
+   * Let's say, for '/' route, we want use a form where user
+   * can enter some data and send it to some other route, say
+   * the route is '/message'.
+   */
+
+  const URL = req.url;
+  if (URL === '/') {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+      <html>
+      <head>
+        <title>Enter Message</title>
+      </head>
+      <body>
+        <form action="/message" method="POST">
+          <input type="text"/>
+          <button type="submit">Send</button>
+        </form>
+      </body>
+      </html>
+    `);
+    
+    /**
+     * NOTE: GET request is automatically sent when we click
+     * a link or enter a URL. A POST request has to be setup 
+     * by us by creating a form (there are other ways too) as
+     * seen above, which will send a POST request to '/message'
+     * route. The good thing about a form is that whenever we
+     * have an input with a `name` attribute, it will also put
+     * the content of the input into the http request header
+     * while sending the POST request.
+     */
+
+    // since we only want to execute the block in L22 once we 
+    // are on '/' route, we should return res.end(), so that 
+    // nothing gets executed after this if statement.
+    return res.end();
+  }
+  
   // https://nodejs.org/api/http.html#http_response_setheader_name_value
   res.setHeader("Content-Type", "text/html");
 
