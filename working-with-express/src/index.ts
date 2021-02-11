090fadb85699f271b37958a6d4d47d98abdcac6e
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2021-02-12 02:04:06 +0530

    Handling Different Routes

--- working-with-express/src/index.ts
+++ working-with-express/src/index.ts
@@ -1,52 +1,79 @@
-// Third Party Imports
-import express, { NextFunction, Request, Response } from 'express';
+import express, {
+  NextFunction,
+  Request,
+  Response,
+} from 'express';
+
+import { PathParams } from 'express-serve-static-core';
 
 const app = express();
-
-app.use((_req: Request, _res: Response, next: NextFunction) => {
-  console.log('In the middleware!');
-  next(); // allows the request to execute the next middleware in line.
-});
-
-// Once we call the next() method inside the middleware in
-// L59, we will be able to execute the next middleware below.
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log('In another middleware!'); // this will be printed in the console -- because next() was called in the previous middleware
-  // Sending response is easier because of express js.
-  // Instead of setting a header and writing to some buffer,
-  // we can use a utility function called `send()` on the 
-  // `response` object.
-  // res.send(): allows the server to send a response, i.e.,
-  res.send('<h1>Hello from Express!</h1>'); // by default, 'Content-Type': text/html
-  // We can manually set the 'Content-Type' header using the res.setHeader() method.
-
-  /**
-   * Therefore, the request to localhost://3000 will NOT die 
-   * now, because we are actually sending a response, which is
-   * an HTML response.
-   * 
-   * This is how we use middleware along with next() or simply
-   * send response using res.send() as shown above.
-   */
-});
+const routes: PathParams = ['/', '/add-product'];
 
 /**
- * Output: [Open http://localhost:3000/]
- * '''''''''''''''''''''''''''''''''''''
- * In the middleware!
- * In another middleware!
+ * app.use() method has 6 other overloads as of express v4.17.1
+ * which means that there are 7 different ways to call it, as 
+ * seen here: https://expressjs.com/en/4x/api.html#app.use and
+ * signature is: app.use([path,] callback [, callback...])
+ * 
+ * In app.use() method, we've an optional parameter which is 
+ * `path` as we can see above in L13. If we don't give the 
+ * `path` parameter, by default express takes it as '/' route.
+ * 
+ * Now, if '/' is used for the app.use() below, then even on 
+ * going to the route '/some-route', the '/' route's middleware
+ * will match. Because '/' doesn't mean that the full path can
+ * only be '/', but it can be any route that starts with '/' -
+ * and because of that, even '/some-route', '/xxxcccdd', etc 
+ * will match the middleware with the path defined as '/'.
  */
 
+// app.use(routes[0], (req: Request, res: Response, next: NextFunction) => {
+//   console.log('In another middleware!');
+//   res.send('<h1>Hello from Express!</h1>');
+// });
+
 /**
- * The idea of using a middleware is a crucial concept when
- * making use of express. Also, we can pass any function that 
- * has the aforementioned syntax ((request, response, next) => {}).
+ * So, middleware (app.use()) with the route '/' will match 
+ * every route in this case. And so, what we can do is, we can
+ * add another middleware before the middleware with '/' route
+ * as shown below. 
+ * 
+ * This works because the JS Engine compiles the JS code from 
+ * top to bottom, and so, whenever a specific route like say 
+ * '/add-product' is matched at the beginning, then there will
+ * be no need to match more middlewares (that can be below 
+ * the currently matched middleware) unless the next() method 
+ * is called in the matched middleware.
+ */
+
+// matches '/add-product' route only
+app.use(routes[1], (_req: Request, res: Response, _next: NextFunction) => {
+  console.log('In \'/add-product\' middleware!');
+  res.send('<h1>The Add Product Page!</h1>');
+});
+
+// matches '/' => matches any route other than '/add-product'
+app.use(routes[0], (_req: Request, res: Response, _next: NextFunction) => {
+  console.log('In \'/\' middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+  // by convention, if a response is being sent as in L58, then
+  // that's also an indication that next() is not supposed to
+  // be called after it.
+});
+
+/**
+ * And so, the order of the middlewares and whether next() is 
+ * being called or not, matters a lot.
  */
 
 app.listen(3000);
 
 /**
- * Now if we run `npm start`, we'll have a running server which doesn't handle any requests for now.
- * The key characteristic of express.js is that it sets up an easy way to handle requests and send responses.
+ * Output: [Open http://localhost:3000/<any route except 'add-product'>]
+ * '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+ * In '/' middleware!
+ * 
+ * Output: [Open http://localhost:3000/add-product]
+ * ''''''''''''''''''''''''''''''''''''''''''''''''
+ * In '/add-product' middleware!
  */
